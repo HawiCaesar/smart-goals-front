ecbd64237f06327da16068b25445c2b69f6ffe66
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-05 22:23:11 +0300

    [Feature] Add functions to handle update bucketlist and modal changes

--- js/containers/BucketlistsContainer.js
+++ js/containers/BucketlistsContainer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
-import { withRouter } from "react-router-dom";
 
-import { get_bucketlists } from "../actions/bucketlistsActions";
-import Bucketlists from "../components/bucketlists"
+import { get_bucketlists, update_bucketlist } from "../actions/bucketlistsActions";
+import Bucketlists from "../components/bucketlists/bucketlists"
 import * as utils from "../utils/tokenUtilities"
 
 
@@ -12,7 +11,7 @@ class BucketlistsContainer extends React.Component{
 
     constructor(props){
         super(props)
-        this.state = { isOpen: false, bucket: 0};
+        this.state = { isOpen: false, isOpen_update: false, bucket: 0, bucket_details:{}};
     }
 
 
@@ -21,7 +20,7 @@ class BucketlistsContainer extends React.Component{
         this.props.get_bucketlists(utils.getAuthToken())
     }
 
-    toggleModal(event){
+    viewItemsModal(event){
         let bucketlist_id = event.currentTarget.getAttribute('data-id');
         this.setState({
             isOpen: !this.state.isOpen,
@@ -29,13 +28,46 @@ class BucketlistsContainer extends React.Component{
         });
     }
 
+    updateBucketlistModal(event){
+
+        let bucketlist_id = event.currentTarget.getAttribute('data-id');
+
+        this.setState({
+            isOpen_update: !this.state.isOpen_update,
+            bucket: bucketlist_id
+        });
+    }
+    onUpdateBucketlist(bucket_id){
+
+        this.props.update_bucketlist(bucket_id, this.state.bucket_details)
+        this.setState({
+            isOpen_update: !this.state.isOpen_update
+        })
+
+    }
+    onChangeBucketlistName(event){
+        this.setState({bucket_details: event.target.value})
+        console.log(this.state.bucket_details)
+    }
+
+    deleteBucketlistModal(event){
+
+        let bucketlist_id = event.currentTarget.getAttribute('data-id');
+
+        // make delete action and reducer to update store and force refresh
+    }
 
     render() {
         return (
             <Bucketlists bucketlists={ this.props.bucketlists_details }
-                         toggleModal={ this.toggleModal.bind(this) }
                          modal_state={ this.state.isOpen }
+                         update_modal_state={ this.state.isOpen_update }
                          bucketlist_id = { this.state.bucket }
+                         viewItemsModal={ this.viewItemsModal.bind(this) }
+                         updateBucketlistModal={ this.updateBucketlistModal.bind(this) }
+                         deleteBucketlistModal={ this.deleteBucketlistModal.bind(this) }
+                         onUpdateBucketlist = { this.onUpdateBucketlist.bind(this)}
+                         onChangeBucketlistName = { this.onChangeBucketlistName.bind(this) }
             />
         );
     }
@@ -49,7 +81,8 @@ function mapStateToProps(state) {
     }
 }
 function mapDispatchToProps(dispatch){
-    return bindActionCreators({get_bucketlists: get_bucketlists}, dispatch);
+    return bindActionCreators({get_bucketlists: get_bucketlists, update_bucketlist: update_bucketlist}, dispatch)
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BucketlistsContainer)
