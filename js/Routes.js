2245194e36d1a229672c8473b828921555e27a8f
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-11 12:23:11 +0300

    [Feature] Add sign up route to routes

--- js/Routes.js
+++ js/Routes.js
@@ -3,6 +3,8 @@ import App from "./components/App";
 import Intro from "./components/Intro";
 import Home from "./containers/home"
 import ItemContainer from "./containers/ItemContainer"
+import BucketlistsContainer from "./containers/BucketlistsContainer"
+import UserContainer from "./containers/UserContainer"
 
 import history from "./history";
 import {
@@ -19,6 +21,8 @@ const Routes = () => {
                 <Route path="/home" component={Home} />
                 <Route path="/intro" component={Intro}/>
                 <Route path="/bucketlist-items" component={ItemContainer}/>
+
+                <Route path="/sign-up" component={UserContainer}/>
             </Switch>
         </Router>
     );
