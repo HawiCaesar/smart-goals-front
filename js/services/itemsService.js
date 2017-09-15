e1f8e796e9143bed0554b5a31a1f0d172bb1aa39
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-15 16:04:23 +0300

    [Feature] Add item service to reduce item actions clutter

---
+++ js/services/itemsService.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import * as utils from "../utils/tokenUtilities"
+
+
+class ItemsService {
+    constructor() {
+
+        const service = axios.create({
+            baseURL: process.env.API_LOCAL_URL,
+
+        });
+
+        service.interceptors.response.use(this.handleSuccess, this.handleError);
+
+
+        this.service = service;
+    }
+
+    handleSuccess(response) {
+        return response;
+    }
+
+    handleError(error) {
+        console.log(error);
+        return Promise.reject(error);
+    }
+
+    post(path, data, callback) {
+        const token = utils.getAuthToken();
+        return this.service.request({
+            method: 'POST',
+            url: path,
+            responseType: 'json',
+            data: {"name": data},
+            headers: { Authorization: `Bearer ${token}` },
+
+        }).then(response => callback(response.status, response.data));
+    }
+
+    get(path, callback) {
+        const token = utils.getAuthToken();
+
+        return this.service.request({
+            method: 'GET',
+            url: path,
+            responseType: 'json',
+            headers: { Authorization: `Bearer ${token}` },
+        }).then(response => callback(response.status, response.data));
+    }
+
+    put(path, data, callback) {
+        const token = utils.getAuthToken();
+        return this.service.request({
+            method: 'PUT',
+            url: path,
+            responseType: 'json',
+            data: {"name": data},
+            headers: { Authorization: `Bearer ${token}` },
+
+        }).then(response => callback(response.status, response.data));
+    }
+
+    delete_service(path, callback) {
+        const token = utils.getAuthToken();
+        return this.service.request({
+            method: 'DELETE',
+            url: path,
+            responseType: 'json',
+            headers: { Authorization: `Bearer ${token}` },
+        }).then(response => callback(response.status, response.data));
+    }
+}
+export default new ItemsService();
