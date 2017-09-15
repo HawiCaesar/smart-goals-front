2a3c219ecbc6c8b32f47b5fb8b8c97d2b2ea1e0e
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-15 15:59:32 +0300

    [Feature] Add item reducer to all reducers

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,13 +1,16 @@
 import { combineReducers } from "redux";
+import { loadingBarReducer } from 'react-redux-loading-bar'
 
 import AuthReducer from "./authReducer"
 import Bucketlists from "./bucketlistsReducer"
-import { loadingBarReducer } from 'react-redux-loading-bar'
+import Item from "./itemReducer"
+
 
 const allReducers = combineReducers({
 
     authenticate: AuthReducer,
     bucketlists: Bucketlists,
+    item: Item,
     loadingBar: loadingBarReducer,
 });
 
