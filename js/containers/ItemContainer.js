5e96953747b76ece7b433d3200c525661c1b0aff
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-15 16:02:54 +0300

    [Feature] Add item Container to render viewItem, add map state & dispatch

---
+++ js/containers/ItemContainer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from "redux";
+import {withRouter} from "react-router-dom";
+
+import HeaderContainer from "./HeaderContainer"
+import ViewItem from "../components/items/viewItem"
+import { get_item } from "../actions/itemActions"
+
+
+class ItemContainer extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false,
+            item_id: this.props.location.state.item_id,
+            bucket_name: this.props.location.state.bucket_name
+        }
+
+    }
+
+    componentWillMount() {
+
+        this.props.get_item(this.props.location.state.bucket_id, this.props.location.state.item_id)
+    }
+
+    render(){
+
+        console.log(this.props.location.state)
+
+        return (
+            <div>
+                <HeaderContainer />
+
+                <ViewItem item_details={this.props.item_details}
+                          item_id={this.state.item_id}
+                          bucket_name={this.state.bucket_name}
+                />
+            </div>
+
+        )
+    }
+
+}
+
+function mapStateToProps(state) {
+    return {
+        item_details: state.item
+
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(
+        {
+            get_item: get_item
+        }, dispatch)
+
+}
+withRouter(ItemContainer)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer)
