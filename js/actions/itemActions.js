77dac308f999495c542838b6fdcdbf6ca0092c5a
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-25 23:52:51 +0300

    [Feature] Add update item and delete using itemservice

--- js/actions/itemActions.js
+++ js/actions/itemActions.js
@@ -1,16 +1,41 @@
-import { showLoading, hideLoading } from 'react-redux-loading-bar'
 import ItemsService from "../services/itemsService"
 
 export function get_item(bucket, item){
 
     return function(dispatch){
         dispatch({type: "FETCH_SPECIFIC_ITEM"});
-        dispatch(showLoading())
 
         ItemsService.get(`/v1/api/bucketlists/${bucket}/items/${item}`, (status, data) =>
             dispatch({type: "ITEM_RESULTS", payload: data })).catch((error) => {
 
             dispatch({type: "FAILED_FETCHING_ITEM", payload: error })
+        });
+
+    }
+}
+
+export function update_item(bucket, item, item_data){
+
+    return function(dispatch){
+
+        ItemsService.put(`/v1/api/bucketlists/${bucket}/items/${item}`, item_data, (status, data) =>
+            dispatch({type: "UPDATED_RESULTS", payload: data })).catch((error) => {
+
+            dispatch({type: "FAILED_UPDATE_ITEM", payload: error })
+        });
+
+    }
+}
+
+export function delete_item(bucket, item){
+
+    return function(dispatch){
+        dispatch({type: "DELETE_ITEM"});
+
+        ItemsService.delete(`/v1/api/bucketlists/${bucket}/items/${item}`, (status, data) =>
+            dispatch({type: "DELETE_RESULTS", payload: data })).catch((error) => {
+
+            dispatch({type: "FAILED_UPDATE_ITEM", payload: error })
         });
 
     }
