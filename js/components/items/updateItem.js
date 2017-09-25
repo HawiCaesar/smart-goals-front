f6918dcff1d2a8094d1e6fee638cc575af591b3e
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-25 23:53:59 +0300

    [Feature] change Update modal to have checkbox

--- js/components/items/updateItem.js
+++ js/components/items/updateItem.js
@@ -8,14 +8,20 @@ class UpdateItem extends React.Component{
                 <h3>Update Item</h3>
 
                 <div className="form-group">
-                    <input className="form-control" type="text" />
+                    <input className="form-control" type="text" defaultValue={this.props.item.item.item_name}
+                    onChange={this.props.onChangeItem} />
                 </div>
 
-                <div className="form-group">
-                    <input className="form-control" type="date" />
+                <div className="checkbox">
+                    <label>
+                        {this.props.item.item.done !== false ?
+                            <input type="checkbox" value="false" defaultChecked onChange={this.props.onChangeItem}/> :
+                            <input type="checkbox" value="true" onChange={this.props.onChangeItem} />
+                        }
+                        Completed This Task</label>
                 </div>
                 <div className="form-group">
-                    <button className="btn btn-primary btn-sm">Update Item</button>
+                    <button className="btn btn-primary btn-sm" onClick={this.props.updateItem}>Update Item</button>
                 </div>
             </div>
         )
