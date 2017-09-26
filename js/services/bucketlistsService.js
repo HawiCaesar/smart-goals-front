ca55b4fba47545d4daa45ccc58e56202c818c4d0
HawiCaesar <brian.odhiambo932@gmail.com>
2017-09-26 19:44:44 +0300

    Refactor: GET bucketlist changed to return only a promise

--- js/services/bucketlistsService.js
+++ js/services/bucketlistsService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import * as utils from "../utils/tokenUtilities"
 
-
 class BucketService {
     constructor() {
         const token = utils.getAuthToken();
@@ -39,7 +38,7 @@ class BucketService {
         }).then(response => callback(response.status, response.data));
     }
 
-    get(path, callback) {
+    get(path) {
         const token = utils.getAuthToken();
 
         return this.service.request({
@@ -47,7 +46,10 @@ class BucketService {
             url: path,
             responseType: 'json',
             headers: { Authorization: `Bearer ${token}` },
-        }).then(response => callback(response.status, response.data));
+        })/*.then(response => {
+            console.log('service response: ', response.data)
+            callback(response.status, response.data)
+        }, error => console.log('service error: ', error));*/
     }
 
     put(path, data, callback) {
@@ -72,4 +74,5 @@ class BucketService {
         }).then(response => callback(response.status, response.data));
     }
 }
+
 export default new BucketService();
